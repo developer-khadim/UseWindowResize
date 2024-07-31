7356dbe95c6f759e9d76dfb0811eb451f1761e0b
Your Name <you@example.com>
2024-07-31 23:55:04 +0500

    weather

--- src/components/Weather web-app/Weather.jsx
+++ src/components/Weather web-app/Weather.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Search from './Search'
 
 const Weather = () => {
+
+  const [search ,setsearch] = useState('');
+  const [loading ,setloading] = useState(false);
+const [WeatherData,setWeatherData] = useState(null);
+
+
+  const handleSearch = async () => {
+const re
+
+  };
+
   return (
     <div>
-        <Search/>
+        <Search
+        search={search}
+        setsearch={setsearch}
+        handleSearch={handleSearch}
+        />
       
     </div>
   )
